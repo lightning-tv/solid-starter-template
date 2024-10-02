4199bd8c554d93c68643971bfbbb9f7e5a8c015c
Chris Lorenzo <chiefcll@gmail.com>
2024-10-02 10:12:32 -0400

    update lock and vite build, fixes duplicate imports

--- vite.config.js
+++ vite.config.js
@@ -6,45 +6,49 @@ import legacy from "@vitejs/plugin-legacy";
 export default defineConfig({
   plugins: [
     hexColorTransform({
-      include: ["src/**/*.{ts,tsx,js,jsx}"],
+      include: ["src/**/*.{ts,tsx,js,jsx}"]
     }),
     solidPlugin({
       solid: {
         moduleName: "@lightningtv/solid",
-        generate: "universal",
-      },
+        generate: "universal"
+      }
     }),
     legacy({
-      targets: ["defaults", "Chrome >= 49"],
+      targets: ["defaults", "Chrome >= 49"]
       // additionalLegacyPolyfills: ["whatwg-fetch", "es6-proxy-polyfill"],
-    }),
+    })
   ],
   resolve: {
     alias: {
-      theme: "@lightningjs/l3-ui-theme-base",
+      theme: "@lightningjs/l3-ui-theme-base"
     },
     dedupe: [
       "solid-js",
       "@lightningtv/solid",
+      "@lightningtv/solid-ui",
       "@lightningtv/core",
-      "@lightningjs/renderer",
-    ],
+      "@lightningjs/renderer"
+    ]
+  },
+  optimizeDeps: {
+    exclude: ["@lightningtv/solid", "@lightningtv/core", "@lightningjs/renderer", "@lightningtv/solid-ui"]
   },
   server: {
     hmr: true,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    }
   },
   test: {
     browser: {
       enabled: true,
       headless: false,
       provider: "playwright",
-      name: "webkit",
+      name: "webkit"
     },
     testTransformMode: { web: ["/.[jt]sx?$/"] },
-    globals: true,
-  },
+    globals: true
+  }
 });
